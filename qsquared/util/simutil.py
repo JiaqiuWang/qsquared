cb624250216652171736a462766026774018543b
pirsquared <sean.snv@gmail.com>
2016-11-19 16:29:29 -0800

    added maximum active draw down

--- qsquared/util/simutil.py
+++ qsquared/util/simutil.py
@@ -203,3 +203,148 @@ def annualized_active_risk(returns, benchmarks):
     axis = 0 if isinstance(benchmarks, pd.Series) else 1
     return annualized_risk(returns.sub(benchmarks, axis=axis))
 
+
+def max_draw_down_absolute(returns):
+    """calculate the maximum draw down of a time series of returns.
+    also include the beginning and end of the draw down period.
+
+    :param returns: time series of returns
+    :type returns: pd.DataFrame
+
+    :rtype: pd.DataFrame
+    """
+    if returns.values.ndim == 1:
+        returns = returns.to_frame()
+    r = (returns.values + 1).cumprod(0)
+    dd = (r / np.maximum.accumulate(r)) - 1
+    mdd = dd.min(0)
+    end = dd.argmin(0)
+    rng = np.arange(len(dd))
+    idx = returns.index.values
+    msk = end >= rng[:, None]
+    beg = np.nanargmax(np.where(msk, r, np.nan), 0)
+    return pd.DataFrame(
+        dict(
+            MaxDrawDown=mdd,
+            Begin=idx[beg],
+            End=idx[end]
+        ),
+        returns.columns)
+
+
+def max_draw_down_relative(returns, benchmarks):
+    """find the magnitude of the largest decline in active
+    return as well as when it began and ended.
+
+    The current draw down is the return from the most recent high point to
+    the current position.  We start by identifying where the most recent high
+    point was for each point in time.  That gets tracked with
+    `argcummax_active`.
+
+    We'll use `argcummax_active` to find out what the cumulative returns
+    and cumulative benchmarks were at those high points.  We'll call these
+    `p0` and `b0`.
+
+    For any two points in time, the active return is define as
+
+    .. math::  \\frac{p}{p_0} - \\frac{b}{b_0}
+
+    This converts to
+
+    .. math:: \\frac{p \cdot b_0 - b \cdot p_0}{p_0 \cdot b_0}
+
+    We use this to calculate the current draw downs and subsequently identify
+    the maximum draw down.
+
+    :param returns: absolute returns
+    :type returns: pd.DataFrame
+
+    :param benchmarks: used to compare against returns
+    :type benchmarks: pd.DataFrame
+
+    :rtype: pd.DataFrame
+    """
+    # re-assign variables for convenience
+    # `p` is for portfolio and `b` is for benchmark
+    p, b = returns.values, benchmarks.values
+    # add 1 and accumulate to get cumulative return
+    # use reshape to ensure both `p` and `b` are 2 dimensional
+    p = (p + 1).cumprod(0).reshape(len(p), -1)
+    b = (b + 1).cumprod(0).reshape(len(b), -1)
+
+    # if `p` and `b` both have one column then `cum_active`
+    # will be one column of active returns. if
+    # `p` is multi-column and `b` is one column, the
+    # subtraction will be broadcast across all columns of `p`.
+    cum_active = p - b
+
+    # keep track of the position where the cumulative maximum
+    # has occured.  We'll use it to slice `p` and `b`
+    argcummax_active = cumargmax(cum_active)
+
+    # arrays of ordinal posistions used for slicing
+    p_col_rng = np.arange(p.shape[1])
+    b_col_rng = np.arange(b.shape[1])
+    row_rng = np.arange(p.shape[0])
+
+    # when a maximum occurs in `cum_active` we need to capture
+    # what `p` and `b` were.  We keep track of that
+    # with `p0` and `b0`.  The track the max because the max
+    # draw down must begin with what ever the max was prior
+    p0 = p[argcummax_active, p_col_rng]
+    b0 = b[argcummax_active, b_col_rng]
+    dd = (p * b0 - b * p0) / (p0 * b0)
+
+    mdd = dd.min(0)
+    end = dd.argmin(0)
+    beg = argcummax_active[end, p_col_rng]
+
+    return pd.DataFrame(
+        dict(
+            Begin=returns.index.values[beg],
+            End=returns.index.values[end],
+            MaxDD=mdd
+        ), returns.columns)
+
+
+def max_draw_down(returns, benchmarks=None):
+    """pass through function.  determine if we need absolute or active draw
+    down.
+
+    :param returns: absolute returns
+    :type returns: pd.DataFrame
+
+    :param benchmarks: used to compare against returns
+    :type benchmarks: pd.DataFrame
+
+    :rtype: pd.DataFrame
+    """
+    if benchmarks is None:
+        return max_draw_down_absolute(returns)
+    else:
+        return max_draw_down_relative(returns, benchmarks)
+
+
+def cumargmax(a, return_cummax=False):
+    """takes a numpy array and returns the cumulative argmax per columns.
+
+    credit @ajcr on stackoverflow.com
+    http://stackoverflow.com/a/40675969/2336654
+
+    :param a: array to calculate cumargmax on
+    :type a: np.ndarray
+
+    :param return_cummax: flag on whether to return the cummax array
+
+    :rtype: np.ndarray
+    """
+    m = np.maximum.accumulate(a)
+    x = np.repeat(
+        np.arange(a.shape[0])[:, None],
+        a.shape[1], axis=1)
+    x[1:] *= m[:-1] < m[1:]
+    np.maximum.accumulate(x, axis=0, out=x)
+    if return_cummax:
+        return x, m
+    else:
+        return x
